d4c5f2bf4de379e1bea8f82f5770ff26bbb1aa2d
Fyk07 <00001138360806sp@al.educacao.sp.gov.br>
2024-05-20 14:14:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 25;
+let raio = diametro / 2;
+
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    circle(xBolinha, yBolinha, diametro);
+    //xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+    
+    if (xBolinha + raio > width || 
+        xBolinha - raio< 0) {
+        velocidadeXBolinha *= -1;
+    }
+    if (yBolinha > height || yBolinha < 0) {
+        velocidadeYBolinha *= -1;
+    }
+}
+
